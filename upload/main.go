fd5ea80c96d368d978295e10e70ec201007163a1
“01super” <fk1024@outlook.com>
2019-12-07 11:02:19 +0800

    完善上传图片基础功能

--- upload/main.go
+++ upload/main.go
@@ -12,19 +12,24 @@ import "io"
 
 import "bytes"
 
+import "log"
+
+import "strings"
+
 func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		v, err := ioutil.ReadFile("./index.html") //读取文件
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w.Write(v) //将文件发送个前端
+		html := loadHTML("./upload.html")
+		w.Write(html)
 	}
 	if r.Method == "POST" {
 		f, h, err := r.FormFile("image")
 		if err != nil {
 			w.Write([]byte("文件上传有误" + err.Error()))
+			return
+		}
+		t := h.Header.Get("Content-Type")
+		if !strings.Contains(t, "image") {
+			io.WriteString(w, "<html><a href=\"/upload\">请上传图片</a></html")
 			return
 		}
 		os.Mkdir("./static", 0666) // 以main.go作为相对路径
@@ -60,12 +65,32 @@ func image(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	name := r.Form.Get("name")
 	html := loadHTML("./img.html")
-	bytes.Replace(html, []byte("@src"), []byte("/image?name="+name), 1)
+	html = bytes.Replace(html, []byte("@src"), []byte("/image?name="+name), 1)
+	w.Write(html)
+}
+
+func listView(w http.ResponseWriter, r *http.Request) {
+	names, err := ioutil.ReadDir("./static/")
+	if err != nil {
+		log.Println("文件读取失败：", err)
+		return
+	}
+	html := loadHTML("./list.html")
+	str := ""
+	for _, v := range names {
+		str += `<li><img src="/image?name=` + v.Name() + `" alt="未发现"></li>`
+	}
+	html = bytes.Replace(html, []byte("@list"), []byte(str), 1)
+	w.Write(html)
+}
+
+func homeView(w http.ResponseWriter, r *http.Request) {
+	html := loadHTML("./index.html")
 	w.Write(html)
 }
 
 func loadHTML(path string) []byte {
-	const errStr string = "<html><head><body><h3>Erros</h3></body></head></html>"
+	const errStr string = "<html><head><body><h3>Errors</h3></body></head></html>"
 	f, err := os.Open(path)
 	if err != nil {
 		return []byte(errStr)
@@ -79,9 +104,15 @@ func loadHTML(path string) []byte {
 }
 
 func main() {
-	http.HandleFunc("/", upload)
+	http.HandleFunc("/", homeView)
+	http.HandleFunc("/upload", upload)
 	http.HandleFunc("/image", imageView)
 	http.HandleFunc("/img", image)
-	fmt.Println("hello")
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/list", listView)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("服务启动失败：", err)
+		return
+	}
+	fmt.Println("服务启动成功... ...")
 }
